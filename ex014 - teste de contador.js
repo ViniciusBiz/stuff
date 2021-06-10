61605d30656b16bacc8a4f241f6da40cdae4c03b
ViniciusBiz <73617519+ViniciusBiz@users.noreply.github.com>
2021-06-10 20:56:11 -0300

    Add files via upload

---
+++ ex014 - teste de contador.js
@@ -0,0 +1,12 @@
+var contador = 1
+while (contador<6){
+    console.log(`repetição ${contador}`)
+    contador++ //vai passar a valer 2 ao inves de 1 e assim por diante
+
+}
+
+/* var contador = 1
+do {
+    console.log(`Passo ${c}`)
+    contador++
+} while (c<=6) */
