e90d6215074e4a93a2927caf1d66381259d0ecf5
ViniciusBiz <73617519+ViniciusBiz@users.noreply.github.com>
2021-06-10 20:38:24 -0300

    Update ex011.js

--- ex011.js
+++ ex011.js
@@ -1,10 +1,11 @@
+// exercício-teste de estruta de decisão
 var idade = 12
 console.log(`você tem ${idade} anos`)
 if (idade < 16) {
     console.log("Não vota")
 }
 else if (idade >= 16 && idade <= 18 || idade >=65) {
-    console.log("vota facultativo")
+    console.log("voto facultativo")
 }
 else {
     console.log("voto obrigatório")
